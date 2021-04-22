5e4fc6523dfffa7899cf5ccd83c5721c0f1d5a26
Jung <rickrl0315db@gmail.com>
2021-04-22 17:49:27 +0900

    server end

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask , render_template ,request ,redirect
+from flask import Flask , render_template ,request ,redirect, send_file
+import matplotlib.pyplot as plt
+import numpy as np
+# 그래프를 이미지로 저장하기 위해 필요한 변환 라이브러리
+from io import BytesIO, StringIO
 
 app = Flask(__name__)
 
@@ -7,7 +11,30 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def index():
   # return "Hello World"
-  return render_template("index.html", data="KIM")
+    return render_template("index.html", data="KIM")
+
+@app.route('/fig/<int:mean>_<int:var>')
+def fig(mean, var):
+    plt.figure(figsize=(4,3))
+    xs = np.random.normal(mean, var, 100) # 평균 분산 n=100
+    ys = np.random.normal(mean, var, 100) 
+    plt.scatter(xs, ys, s=100, marker='h', color='red',alpha=0.3)
+
+    img = BytesIO()
+    plt.savefig(img, format='png', dpi=200)
+
+    # 한번쓰고 처음으로 돌아가게 만드는 것
+    img.seek(0)
+    print(xs)
+    return send_file(img , mimetype='image/png')
+
+
+@app.route('/normal/<m_v>')
+def normal(m_v):
+    m , v = m_v.split('_')
+    m , v = int(m) ,int(v)
+    return render_template("random_gen.html", mean=m, var=v, width=100, height=100)
+    
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
