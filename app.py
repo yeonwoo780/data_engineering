4256ac8454f0226517f0ce114984523d99d1fa2d
Jung <rickrl0315db@gmail.com>
2021-04-22 16:45:01 +0900

    server index rendering

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 app.debug = True
 
+@app.route('/', methods=['GET'])
+def index():
+  # return "Hello World"
+  return render_template("index.html", data="KIM")
+
 if __name__ == '__main__':
   app.run()
